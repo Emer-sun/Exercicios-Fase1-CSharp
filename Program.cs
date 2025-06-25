3e36d8934434a60bf6719174bd4498e78b04add2
Emer-sun <otsutsuki_maroki@hotmail.com>
2025-06-25 20:28:43 -0300

    Primeiro commit - Exercícios Fase 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Exercicios - Fase 1!");
+
+int idade = 35;
+string nome = "Maria";
+float altura = 3.45f;
+DateTime data = new DateTime(1999, 09, 04);
+const int ano = 12;
+double? nota = 7.80;
+
+int x = 77;
+int y = 66;
+
+
+
+Console.WriteLine("Idade: " + idade);
+Console.WriteLine("Nome: " + nome);
+Console.WriteLine("Altura: " + altura + "m");
+Console.WriteLine("Data aleatória: " + data.ToShortDateString());
+Console.WriteLine("Ano letivo: " + ano);
+Console.WriteLine("Nota: " + nota);
+Console.WriteLine("Soma de x + y: " + (x + y));
+
+
